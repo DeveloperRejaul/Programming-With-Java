5f18050a0f52ab430c59a760cb41a3c9defc5de4
rejaul <devrejaul.official@gmail.com>
2023-09-30 12:59:53 +0600

    add

---
+++ 03.javaOutput/Main.java
@@ -0,0 +1,15 @@
+public class Main {
+
+    public static void main(String[] args){
+
+        // text print 
+        System.out.println("Hello World"); // new line
+        System.out.print("Hello World! "); // single line
+        System.out.print("I will print on the same line."); // single line
+
+        // number print
+        System.out.print(20);
+        System.out.println(100+10);
+    }
+
+}
---
+++ 04.javacomments/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main(String[] args){
+        // single line comments
+        /*
+        * mulley line comments  
+        */
+        System.out.println("Hello World");
+
+    }
+}
